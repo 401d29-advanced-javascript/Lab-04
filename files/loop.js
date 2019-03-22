9b2fe63df36d91962ded1eb7985e080e50d6d9aa
AaronBruce <aaron.bruce@comcast.net>
2019-03-21 19:59:01 -0700

    finished task 1 and started task 2

---
+++ files/loop.js
@@ -0,0 +1,161 @@
+'use strict';
+
+var fs = require('fs');
+const os = require('os');
+
+
+console.log( os.endianness() );
+
+let buffer1 = new Buffer([67, 111, 114, 121]);
+let buffer2 = new Buffer([65, 97, 114, 111, 110]);
+let buffer3 = new Buffer([83, 104, 97, 103, 103, 121]);
+
+let contents = [buffer1, buffer2, buffer3];
+
+contents.forEach(buffer => {
+  console.log('Buffer AFTER forEach(): ', buffer.toString());
+});'use strict';
+
+var fs = require('fs');
+const os = require('os');
+
+
+console.log( os.endianness() );
+
+let buffer1 = new Buffer([67, 111, 114, 121]);
+let buffer2 = new Buffer([65, 97, 114, 111, 110]);
+let buffer3 = new Buffer([83, 104, 97, 103, 103, 121]);
+
+let contents = [buffer1, buffer2, buffer3];
+
+contents.forEach(buffer => {
+  console.log('Buffer AFTER forEach(): ', buffer.toString());
+});'use strict';
+
+var fs = require('fs');
+const os = require('os');
+
+
+console.log( os.endianness() );
+
+let buffer1 = new Buffer([67, 111, 114, 121]);
+let buffer2 = new Buffer([65, 97, 114, 111, 110]);
+let buffer3 = new Buffer([83, 104, 97, 103, 103, 121]);
+
+let contents = [buffer1, buffer2, buffer3];
+
+contents.forEach(buffer => {
+  console.log('Buffer AFTER forEach(): ', buffer.toString());
+});'use strict';
+
+var fs = require('fs');
+const os = require('os');
+
+
+console.log( os.endianness() );
+
+let buffer1 = new Buffer([67, 111, 114, 121]);
+let buffer2 = new Buffer([65, 97, 114, 111, 110]);
+let buffer3 = new Buffer([83, 104, 97, 103, 103, 121]);
+
+let contents = [buffer1, buffer2, buffer3];
+
+contents.forEach(buffer => {
+  console.log('Buffer AFTER forEach(): ', buffer.toString());
+});'use strict';
+
+var fs = require('fs');
+const os = require('os');
+
+
+console.log( os.endianness() );
+
+let buffer1 = new Buffer([67, 111, 114, 121]);
+let buffer2 = new Buffer([65, 97, 114, 111, 110]);
+let buffer3 = new Buffer([83, 104, 97, 103, 103, 121]);
+
+let contents = [buffer1, buffer2, buffer3];
+
+contents.forEach(buffer => {
+  console.log('Buffer AFTER forEach(): ', buffer.toString());
+});'use strict';
+
+var fs = require('fs');
+const os = require('os');
+
+
+console.log( os.endianness() );
+
+let buffer1 = new Buffer([67, 111, 114, 121]);
+let buffer2 = new Buffer([65, 97, 114, 111, 110]);
+let buffer3 = new Buffer([83, 104, 97, 103, 103, 121]);
+
+let contents = [buffer1, buffer2, buffer3];
+
+contents.forEach(buffer => {
+  console.log('Buffer AFTER forEach(): ', buffer.toString());
+});'use strict';
+
+var fs = require('fs');
+const os = require('os');
+
+
+console.log( os.endianness() );
+
+let buffer1 = new Buffer([67, 111, 114, 121]);
+let buffer2 = new Buffer([65, 97, 114, 111, 110]);
+let buffer3 = new Buffer([83, 104, 97, 103, 103, 121]);
+
+let contents = [buffer1, buffer2, buffer3];
+
+contents.forEach(buffer => {
+  console.log('Buffer AFTER forEach(): ', buffer.toString());
+});'use strict';
+
+var fs = require('fs');
+const os = require('os');
+
+
+console.log( os.endianness() );
+
+let buffer1 = new Buffer([67, 111, 114, 121]);
+let buffer2 = new Buffer([65, 97, 114, 111, 110]);
+let buffer3 = new Buffer([83, 104, 97, 103, 103, 121]);
+
+let contents = [buffer1, buffer2, buffer3];
+
+contents.forEach(buffer => {
+  console.log('Buffer AFTER forEach(): ', buffer.toString());
+});'use strict';
+
+var fs = require('fs');
+const os = require('os');
+
+
+console.log( os.endianness() );
+
+let buffer1 = new Buffer([67, 111, 114, 121]);
+let buffer2 = new Buffer([65, 97, 114, 111, 110]);
+let buffer3 = new Buffer([83, 104, 97, 103, 103, 121]);
+
+let contents = [buffer1, buffer2, buffer3];
+
+contents.forEach(buffer => {
+  console.log('Buffer AFTER forEach(): ', buffer.toString());
+});'use strict';
+
+var fs = require('fs');
+const os = require('os');
+
+
+console.log( os.endianness() );
+
+let buffer1 = new Buffer([67, 111, 114, 121]);
+let buffer2 = new Buffer([65, 97, 114, 111, 110]);
+let buffer3 = new Buffer([83, 104, 97, 103, 103, 121]);
+
+let contents = [buffer1, buffer2, buffer3];
+
+contents.forEach(buffer => {
+  console.log('Buffer AFTER forEach(): ', buffer.toString());
+});
